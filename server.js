6e2867a1d535903ea15ebd1cf12ebb936ba52d29
ayush99-ai <ayushpardeshi8888@gmail.com>
2026-07-10 12:42:52 +0530

    Refactor server.js for improved structure

--- server.js
+++ server.js
@@ -9,20 +9,16 @@ const postRoutes = require("./routes/postRoutes");
 
 const app = express();
 
-// middleware
 app.use(cors());
 app.use(express.json());
 
-// routes
 app.use("/users", userRoutes);
 app.use("/posts", postRoutes);
 
-// basic route
 app.get("/", (req, res) => {
   res.send("Server is running");
 });
 
-// mongo connection (FIXED PROPERLY)
 mongoose
   .connect(process.env.MONGO_URL)
   .then(() => {
@@ -32,7 +28,6 @@ mongoose
     console.log("MongoDB connection error:", err);
   });
 
-// start server
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
